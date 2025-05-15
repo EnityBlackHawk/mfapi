a1b0a6da0dfb545b0722730db341c85c6a2d1294
Luan Felipe Marmentini <luanf2003@gmail.com>
2025-05-14 22:50:36 -0300

    feat (service): Setup - AccquireMetadaStep

--- src/main/java/org/example/mfapi/dto/controller/MainController.java
+++ src/main/java/org/example/mfapi/dto/controller/MainController.java
@@ -1,19 +1,27 @@
 package org.example.mfapi.dto.controller;
 
 import org.example.mfapi.dto.SetupDTO;
+import org.example.mfapi.service.MfService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.utfpr.mf.migration.params.MetadataInfo;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
+    private final MfService mfService;
+
+    public MainController(MfService mfService) {
+        this.mfService = mfService;
+    }
+
     @PostMapping(path = "/setup")
-    public ResponseEntity<SetupDTO> setup(@RequestBody SetupDTO dto) {
-        return  ResponseEntity.ok(dto);
+    public ResponseEntity<MetadataInfo> setup(@RequestBody SetupDTO dto) {
+        return  ResponseEntity.ok(mfService.setup(dto));
     }
 
 
--- src/main/java/org/example/mfapi/service/MfService.java
+++ src/main/java/org/example/mfapi/service/MfService.java
@@ -1,17 +1,65 @@
 package org.example.mfapi.service;
 
+import org.example.mfapi.dto.SetupDTO;
 import org.springframework.stereotype.Service;
+import org.utfpr.mf.descriptor.CachePolicy;
+import org.utfpr.mf.descriptor.MfMigratorDesc;
+import org.utfpr.mf.enums.DefaultInjectParams;
+import org.utfpr.mf.interfaces.IMfMigrationStep;
+import org.utfpr.mf.migration.MfMigrationStepFactory;
 import org.utfpr.mf.migration.MfMigrator;
+import org.utfpr.mf.migration.params.MetadataInfo;
+import org.utfpr.mf.migration.params.MigrationSpec;
+import org.utfpr.mf.model.Credentials;
+import org.utfpr.mf.prompt.Framework;
+
+import java.util.List;
 
 @Service
 public class MfService {
 
+    private MfMigrationStepFactory factory = new MfMigrationStepFactory();
+    private MfMigrator.Binder binder = new MfMigrator.Binder();
+    private MfMigrator migrator;
 
-    void createMigrator() {
+    public MetadataInfo setup(SetupDTO dto) {
+
+        MigrationSpec spec = MigrationSpec.builder()
+                .framework(Framework.SPRING_DATA)
+                .allow_ref(dto.getPreferences().getAllowRef())
+                .name(dto.getProjectName())
+                .prioritize_performance(dto.getPreferences().getPreferPerformance())
+                .reference_only(false)
+                .workload(dto.getWorkloads())
+                .build();
+
+        binder.bind(DefaultInjectParams.MIGRATION_SPEC.toString(), spec);
+        binder.bind(DefaultInjectParams.LLM_KEY.toString(), dto.getLlm().getApiKey());
 
 
+
+        IMfMigrationStep step = factory.createAcquireMetadataStep();
+
+        MfMigratorDesc desc = new MfMigratorDesc();
+        desc.binder = binder;
+        desc.steps = List.of(step);
+        desc.llmServiceDesc.llm_key = dto.getLlm().getApiKey();
+        desc.llmServiceDesc.model = dto.getLlm().getModel();
+        desc.llmServiceDesc.cachePolicy = CachePolicy.DEFAULT;
+        desc.llmServiceDesc.cacheDir = "cache";
+
+        migrator = new MfMigrator(desc);
+
+        Credentials credentials = new Credentials(
+                dto.getRdbAccess().getHost(),
+                dto.getRdbAccess().getUser(),
+                dto.getRdbAccess().getPassword()
+        );
+
+        return (MetadataInfo) migrator.execute(credentials);
 
     }
+
 
 
 }
